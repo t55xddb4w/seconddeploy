9b468e35ddec1394c998ce08db2650c9e587e451
t55xddb4w <85150156+t55xddb4w@users.noreply.github.com>
2021-06-01 18:52:10 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,11 +3,17 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-gChromeOptions = webdriver.ChromeOptions()
-gChromeOptions.add_argument("window-size=1920x1480")
-gChromeOptions.add_argument("disable-dev-shm-usage")
-gDriver = webdriver.Chrome(
-    chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-)
-gDriver.get("https://index.strongerdc.ml")
-time.sleep(100)
+check=1
+while(check>0):
+    gChromeOptions = webdriver.ChromeOptions()
+    gChromeOptions.add_argument("window-size=1920x1480")
+    gChromeOptions.add_argument("disable-dev-shm-usage")
+    gDriver = webdriver.Chrome(
+        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
+    )
+    
+    gDriver.get("https://index.strongerdc.ml")
+    time.sleep(600)
+    gDriver.close()
+    print("Times Run = ", check)
+    check=check+1
