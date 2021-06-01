15ce2aa0de636b17a711a6a950eacc3f08b1130c
t55xddb4w <85150156+t55xddb4w@users.noreply.github.com>
2021-06-01 19:19:50 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,21 +2,21 @@ import time
 import os
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary #We import this so we can specify the Firefox browser binary location
 
 weburl = os.getenv('WEB_URL')
+FF_options = webdriver.FirefoxOptions()
+FF_profile = webdriver.FirefoxProfile()
+FF_options.add_argument("-headless")
+FF_profile.update_preferences()
 
 check=1
 while(check>0):
-    gChromeOptions = webdriver.ChromeOptions()
-    gChromeOptions.add_argument("window-size=1920x1480")
-    gChromeOptions.add_argument("disable-dev-shm-usage")
-    gDriver = webdriver.Chrome(
-        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
-    )
-    
-    gDriver.get(weburl)
-    time.sleep(600)
-    gDriver.close()
+    driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile, executable_path=os.environ.get("GECKODRIVER_PATH"), firefox_binary=FirefoxBinary(os.environ.get("FIREFOX_BIN")))
+    driver.get("https://icanhazip.com")
+    ipaddr = driver.page_source
+    print(ipaddr)
+    time.sleep(10)
+    driver.close()
     print("Times Run = ", check)
     check=check+1
